c94d29e0b72b899bedd8c2e09e7bda5ecbe23d02
Aluno <renanandreolla@outlook.com>
2019-08-14 20:11:03 -0300

    Verifica se o número digitado é positivo ou negativo

--- js/index.js
+++ js/index.js
@@ -1,2 +1,13 @@
 //Crie uma função que receba um valor e informe se ele é positivo
 //ou não.
+
+var value = parseInt(prompt("Digite um valor: "));
+
+function check (value) {
+  if (value >= 0) {
+    return document.body.append("Número positivo");
+  }else if(value < 0){
+    return document.body.append("Número nagativo");
+  }
+}
+check(value);
